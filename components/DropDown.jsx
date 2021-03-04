7010b0d0cac0c5dbc95304c2870359264ff98930
Yythom <1078013818@qq.com>
2021-03-04 15:57:29 +0800

    fix

--- components/DropDown.jsx
+++ components/DropDown.jsx
@@ -1,18 +1,16 @@
 
-import React from 'react';
-import { View, Image, Text } from '@tarojs/components';
-
+import React, { useState } from 'react';
+import { View } from '@tarojs/components';
 
 import './styles/dropdown.scss';
-import { useState } from 'react';
 
 function DropDown(props) {
-    let { className, list, setList, onClick, } = props;
+    let { className, title } = props;
     const [flag, setFlag] = useState(false);
 
     return (
         <View className={`drop_wrap ${className}`}>
-            <View className='drop_title' onClick={() => { setFlag(!flag) }}>测试下拉</View>
+            <View className='drop_title' onClick={() => { setFlag(!flag) }}>{title || '请定义标题'}</View>
             <View className='square' style={!flag ? { height: 0 } : { height: props.children?.props?.style?.height || 0 }}>
                 {props.children}
             </View>
