e72f4e4bb8d2cdc5cb80e5e6070ac2ab5dd7c3a1
Yythom <1078013818@qq.com>
2021-06-02 15:54:39 +0800

    fix

--- src/pages/test/sort.jsx
+++ src/pages/test/sort.jsx
@@ -10,11 +10,6 @@ const Sort = () => {
         " 2 ",
         " 3 ",
         " 4 ",
-        " 5 ",
-        " 6 ",
-        " 7 ",
-        " 8 ",
-        " 9 ",
     ]);
     const [pageInfo, setPageInfo] = useState(
         {
